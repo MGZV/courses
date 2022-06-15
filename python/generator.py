79a50620ad754cbd4acb56d4365fea13220a08c6
MGZV <anr.mgzv@gmail.com>
2022-06-15 19:44:45 +0300

    add: python generators

--- python/generator.py
+++ python/generator.py
@@ -1,4 +1,7 @@
 # Объект генератора
+import sys
+from datetime import datetime
+
 
 def func(n):
     res = []
@@ -36,9 +39,68 @@ def func_3(n):
 # print("разделитель между обращениями")
 # print(next(c))
 
-d = func_3(10)
-print(next(d))
-print(next(d))
-print(next(d))
-print(next(d))
+# d = func_3(10)
+# print(next(d))
+# print(next(d))
+# print(next(d))
+# print(next(d))
+
+
+# start = datetime.now()
+# print(f"Start working at {start}")
+# d = func_3(100_000_000)
+# l = func(100_000_000)
+# r = range(100_000_000)
+#
+# print(f"size of object: {sys.getsizeof(r)}")
+# print(f"Finish at: {datetime.now()}. Working time: {datetime.now() - start}")
+
+# d = func_3(10)
+# print(3 in d)
+# print(list(d))
+
+
+# def gen_f():
+#     yield 1
+#     yield 2
+#
+# gen_o = gen_f()
+# print(gen_o)
+#
+# iterator = iter(gen_o)
+# print(iterator)
+
+
+l = [1, 2, 3, 4]
+# print(next(l))
+# list_it = iter(l)
+# print(next(list_it))
+# print(next(list_it))
+# print(next(list_it))
+# print(next(list_it))
+# print(next(list_it))
+
+# Что такое for
+try:
+    iterator = iter(l)
+    while True:
+        print(next(iterator))
+except StopIteration:
+    pass
+
+
+
+
+
+# for el in l:
+#     print(el)
+
+
+
+
+
+
+
+
+
 
